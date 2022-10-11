e72f606faf61f732b979b2ef337a0863356d0060
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-10-11 10:14:35 -0300

    adicionado botão de copy

--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -7,47 +7,79 @@ export const Forms = () => {
   const [inputChars, setInputChars] = React.useState("");
   const [outputChars, setOutputChars] = React.useState("");
 
+  const clearAllInputs = () => {
+    setEnterText("");
+    setInputChars("");
+    setOutputChars("");
+  };
+
+  const copyToClipboard = (text: string) => {
+    navigator.clipboard.writeText(text);
+  };
+
   return (
-    <div>
+    <>
       <h2>Troque caracteres por outros</h2>
-      <form className={styles.form}>
-        <div className={styles.inputFields}>
-          <div>
-            <label htmlFor="entrada">Entrada</label>
-            <input
-              type="text"
-              id="entrada"
-              onChange={(e) => setInputChars(e.target.value)}
-              value={inputChars}
-            />
-          </div>
-          <div>
-            <label htmlFor="saida">Saída</label>
-            <input
-              type="text"
-              id="saida"
-              onChange={(e) => setOutputChars(e.target.value)}
-              value={outputChars}
-            />
-          </div>
-        </div>
-      </form>
-      <div className={styles.container}>
+
+      <section className={styles.inputFields}>
         <div>
-          <label htmlFor="">texto de entrada</label>
+          <label htmlFor="entrada">Entrada</label>
+          <input
+            type="text"
+            id="entrada"
+            placeholder="a"
+            onChange={(e) => setInputChars(e.target.value)}
+            value={inputChars}
+          />
+        </div>
+        <div>
+          <label htmlFor="saida">Saída</label>
+          <input
+            type="text"
+            id="saida"
+            placeholder="aaa"
+            onChange={(e) => setOutputChars(e.target.value)}
+            value={outputChars}
+          />
+        </div>
+      </section>
+
+      <section className={styles.textFields}>
+        <div>
+          <label htmlFor="texto-entrada">Texto de entrada</label>
           <textarea
+            id="texto-entrada"
+            placeholder="abacate"
             value={enterText}
             onChange={(e) => setEnterText(e.target.value)}
           />
+          <button
+            onClick={() => copyToClipboard(enterText)}
+            title="Copiar texto do campo"
+          >
+            Copiar
+          </button>
         </div>
         <div>
-          <label htmlFor="">texto de saída</label>
+          <label>Texto de saída</label>
           <textarea
+            placeholder="aaabaaacaaate"
             value={enterText.split(inputChars).join(outputChars)}
             readOnly
           />
+          <button
+            onClick={() =>
+              copyToClipboard(enterText.split(inputChars).join(outputChars))
+            }
+            title="Copiar texto do campo"
+          >
+            Copiar
+          </button>
         </div>
-      </div>
-    </div>
+      </section>
+      <button onClick={clearAllInputs} title="Limpar todos os campos">
+        Limpar tudo
+      </button>
+    </>
   );
 };
